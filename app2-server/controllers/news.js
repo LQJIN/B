b78d1af64e3b8b5b48bcff71d30509f6e4927fb1
LQJIN <isjinliqiang@foxmail.com>
2018-05-10 20:57:29 +0800

    update app2-server controllers news.js

--- app2-server/controllers/news.js
+++ app2-server/controllers/news.js
@@ -2,8 +2,8 @@ var news_model = require('../models/news_model.js');
 
 exports.index = function (req,res,next) {
     /*res.render('index',{title:'Express'});*/
-    /*res.send('跨域请求，请求所有的文章数据。');*/
+    //res.send('跨域请求，请求所有的文章数据。');
     news_model.getAllNews(function (results) {
-        res.json(results);
+        return res.json(results);
     });
 };
