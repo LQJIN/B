7e561ca38ed77ac7b207735d981f2c610b33e8c1
LQJIN <isjinliqiang@foxmail.com>
2018-05-11 21:10:53 +0800

    add getNewsDetail function

--- app2-server/models/news_model.js
+++ app2-server/models/news_model.js
@@ -1,6 +1,11 @@
 var db = require('./db.js');
 
 exports.getAllNews = function (callback) {
-    var sql = "select * from t_news";
+    var sql = "select * from news";
     db.query(sql,[],callback);
+};
+
+exports.getNewsDetail = function (newsId, callback) {
+    var sql = "select * from news where nid = ? ";
+    db.query(sql,[newsId],callback);
 };
