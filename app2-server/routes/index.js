f495004e4f4c13fe9cf35184255016ddf5ee59df
LQJIN <isjinliqiang@foxmail.com>
2018-05-11 21:11:50 +0800

    add router/newsDetail

--- app2-server/routes/index.js
+++ app2-server/routes/index.js
@@ -8,6 +8,10 @@ var news = require('../controllers/news.js');
 /* GET home page. */
 router.get('/index', news.index);
 
+/* GET news detail page */
+router.get('/newsDetail', news.detail);
+
+
 /* GET reg page. */
 router.get('/reg',user.reg);
 
