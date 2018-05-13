0ceae3488231c2de12dca2b6d8721eeb68ebfa3e
LQJIN <isjinliqiang@foxmail.com>
2018-05-13 20:34:43 +0800

    change width

--- app-client/src/components/common/SlideHorizontal.vue
+++ app-client/src/components/common/SlideHorizontal.vue
@@ -33,8 +33,8 @@
     position: fixed; top: 0.6rem;}
   /*.carousel { overflow-x: scroll;}
   设置导航栏左右滚动，在滚动元素ul的父元素设置overflow-x: scroll;*/
-  .slide-horizontal .carousel-tag { width: 8.2rem; height: 0.6rem; line-height: 0.6rem;}
+  .slide-horizontal .carousel-tag { width: 7.2rem; height: 0.6rem; line-height: 0.6rem;}
   .slide-horizontal ul li {float: left; height: 0.6rem; text-align: center;
-    color: #ffffff; font-size: 0.3rem; padding: 0 0.2rem;}
+    color: #ffffff; font-size: 0.25rem; padding: 0 0.2rem;}
   .active {background: #00446e;}
 </style>
