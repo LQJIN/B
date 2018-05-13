6dc291af8843dab691e877b99c4eb380ff15e681
LQJIN <isjinliqiang@foxmail.com>
2018-05-13 20:35:54 +0800

    change icon

--- app-client/src/components/common/Head.vue
+++ app-client/src/components/common/Head.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="head">
     <ul>
-      <li><p class="icon">NewsDaily</p></li>
-      <li><span class="icon icon-search"></span></li>
-      <li><router-link to="/signin"><span class="icon icon-user"></span></router-link></li>
+      <li><p class="fa">NewsDaily</p></li>
+      <li><span class="fa fa-search"></span></li>
+      <li><router-link to="/signin"><span class="fa fa-user"></span></router-link></li>
     </ul>
   </div>
 </template>
@@ -15,14 +15,14 @@
   .head ul {display: flex; width: 100%;}
   .head ul li {text-align: center;}
   .head ul li:nth-child(1){flex: 8; float: left; text-align: left;
-    font-size: 0.3rem; padding-left: 0.4rem; }
+    font-size: 0.25rem; padding-left: 0.3rem; }
   .head ul li:nth-child(2){flex: 1; float: right; }
   .head ul li:nth-child(3){flex: 1; float: right; }
 
-  .head ul li .icon { color: #c9c9c9;}
+  .head ul li .fa { color: #c9c9c9;}
 
   .head span{ width: 0.6rem; height: 0.6rem; line-height: 0.6rem;
-    text-align:center; float: right; font-size: 0.3rem; color: #ffffff; }
+    text-align:center; float: right; font-size: 0.25rem; color: #ffffff; }
 </style>
 
 <script>
