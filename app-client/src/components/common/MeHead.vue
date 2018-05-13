1c8cc921a4a23bd19421aac0561f373e895481a1
LQJIN <isjinliqiang@foxmail.com>
2018-05-13 20:47:26 +0800

    change icon

--- app-client/src/components/common/MeHead.vue
+++ app-client/src/components/common/MeHead.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="me-head">
-    <h2>Me</h2>
+    <i v-on:click="toIndex"><span class="fa fa-angle-left"></span></i>
+    <h2>Sign In</h2>
     <slot>
       <router-link to="/settings">
         <span class="glyphicon glyphicon-cog"></span>
@@ -8,11 +9,19 @@
     </slot>
   </div>
 </template>
-
+<script>
+  export default {
+    methods:{
+      toIndex:function(){
+        history.back();
+      },
+    }
+  }
+</script>
 <style scoped>
-  .me-head { display: flex; height: 1rem; color: #fff; position: fixed; top: 0; width: 100%;
-              background: #1649a9; border-bottom: 0.01rem solid #a2b7b8; z-index: 1;}
-  .me-head span{ width: 1rem; height: 1rem; line-height: 1rem; text-align:center; font-size: 0.5rem; color: #ffffff; }
-  .me-head h2 { flex: 1; text-align: center; text-indent: 1.2rem; line-height: 1rem; font-size: 0.5rem;}
+  .me-head { display: flex; height: 0.6rem; color: #fff; position: fixed; top: 0; width: 100%;
+              background: #ffffff; border-bottom: 0.01rem solid #e7e7e7; z-index: 1;}
+  .me-head span{ width: 0.8rem; height: 0.6rem; line-height: 0.6rem; text-align:center; font-size: 0.3rem; color: #c9c9c9; }
+  .me-head h2 { flex: 1; text-align: center; text-indent: 0rem; line-height: 0.6rem; font-size: 0.3rem; color: #000000;}
 </style>
 
