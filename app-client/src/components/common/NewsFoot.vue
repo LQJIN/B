1117c85b281bd1e3da0435b9e06ed9e36c0a45ab
LQJIN <isjinliqiang@foxmail.com>
2018-05-13 20:36:28 +0800

    change icon

--- app-client/src/components/common/NewsFoot.vue
+++ app-client/src/components/common/NewsFoot.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="news-foot">
-    <ul>
+    <ul class="foot-list">
       <li>
         <a v-on:click="msgAdd()">
           <h5>{{msg}}</h5>
@@ -25,16 +25,26 @@
     <div class="popup">
       <div class="popup-content">
         <ul class="other-account-list" >
-          <li><div class="block"><img src="../../assets/img/twitter.png" alt="Twitter"><p>Twitter</p></div></li>
-          <li><div class="block"><img src="../../assets/img/weibo.png" alt="Weibo"><p>Weibo</p></div></li>
-          <li><div class="block"><img src="../../assets/img/wechat.png" alt="WeChat"><p>WeChat</p></div></li>
-          <li><div class="block"><img src="../../assets/img/Moments.png" alt="weibo"><p>WeChat Moments</p></div></li>
-          <li><div class="block"><img src="../../assets/img/mail.png" alt="Mail"><p>Mail</p></div></li>
-          <li><div class="block"><img src="../../assets/img/message.png" alt="Message"><p>Message</p></div></li>
-          <li><div class="block"><img src="../../assets/img/browser.png" alt="Browser"><p>Browser</p></div></li>
-          <li><div class="block"><img src="../../assets/img/FaceBook.png" alt="FaceBook"><p>FaceBook</p></div></li>
+          <li>
+            <div class="block">
+              <img src="../../assets/img/message.png" alt="Twitter">
+              <i>Message</i>
+            </div>
+          </li>
+          <li><div class="block"><img src="../../assets/img/weibo.png" alt="Weibo"><i>Weibo</i></div></li>
+          <li><div class="block"><img src="../../assets/img/wechat.png" alt="WeChat"><i>WeChat</i></div></li>
+          <li><div class="block"><img src="../../assets/img/Moments.png" alt="weibo"><i>Moments</i></div></li>
+          <li><div class="block"><img src="../../assets/img/mail.png" alt="Mail"><i>Mail</i></div></li>
+          <li><div class="block"><img src="../../assets/img/twitter.png" alt="Message"><i>Twitter</i></div></li>
+          <li><div class="block"><img src="../../assets/img/browser.png" alt="Browser"><i>Browser</i></div></li>
+          <li>
+            <div class="block">
+              <img src="../../assets/img/FaceBook.png" alt="FaceBook">
+              <i>FaceBook</i>
+            </div>
+          </li>
         </ul>
-        <button class="close">Cancel</button>
+        <button class="close">Close</button>
       </div>
       <!--<div class="popup-bg"></div>-->
     </div>
@@ -73,9 +83,9 @@
 <style scoped>
   .news-foot{position:fixed; width: 100%; bottom: 0; }
 
-  ul {display: flex; height: 0.6rem;line-height: 0.6rem;}
-  ul li {flex: 1;  background-color: #ffffff; text-align: center; font-size: 0.3rem;}
-  ul li:nth-child(3){padding-top: 0.02rem;}
+  .foot-list {display: flex; height: 0.6rem;line-height: 0.6rem;}
+  .foot-list li {flex: 1;  background-color: #ffffff; text-align: center; font-size: 0.3rem;}
+  .foot-list li:nth-child(3){padding-top: 0.02rem;}
   .news-foot a{display: inline-block; width: 100%;height: 100%; color: #1c4d9c; text-align:center;}
 
   .news-foot span{width: 0.44rem; height: 0.6rem; line-height: 0.6rem;  font-size: 0.3rem;}
@@ -86,16 +96,21 @@
 
   /*弹出图层*/
   .other-account-list{ display: flex; display: -webkit-flex; /* Safari */ flex-wrap: wrap;  text-align: center;}
-  .other-account-list div{width: 1.1rem; height: 1.4rem; padding: 0.2rem 0.2rem 0 0.2rem }
-  .other-account-list img{ width: 1rem; height: 1rem;}
-  .other-account-list p{ display: block; width: 1.1rem ; height: 0.3rem; font-size: 0.15rem}
-  .other-account-list ul li:nth-child(1){flex: 1; }
-  .other-account-list ul li:nth-child(2){flex: 1; }
-  .other-account-list ul li:nth-child(3){flex: 1; }
-  .other-account-list ul li:nth-child(4){flex: 1; }
-  .other-account-list ul li:nth-child(5){flex: 1; }
-  .other-account-list ul li:nth-child(6){flex: 1; }
+  .other-account-list li { background: #f0f0f0; text-align: center; padding-top: 0.1rem;}
 
+  .block {display: inline-block; width: 1.3rem; height: 1.3rem; background-color: #f0f0f0;}
+  .block img{ width: 0.8rem; height: 0.8rem;position: absolute;}
+  .block i{ font-size: 0.2rem; position: relative; top: 0.7rem;}
+
+  .other-account-list li:nth-child(1){flex: 1; margin-left: 0.18rem;}
+  .other-account-list li:nth-child(2){flex: 1; }
+
+  .other-account-list li:nth-child(3){flex: 1; }
+  .other-account-list li:nth-child(4){flex: 1; }
+  .other-account-list li:nth-child(5){flex: 1; }
+  .other-account-list li:nth-child(6){flex: 1; }
+  .other-account-list li:nth-child(7){flex: 1; }
+  .other-account-list li:nth-child(8){flex: 1; }
   .popup {transition: all .5s; -webkit-transition: all .5s; visibility: hidden; opacity: 0;}
   .popup.show{visibility: visible; opacity: 1;}
   .popup {position: fixed; top:0; left: 0; width: 100%; height: 100%;}
@@ -104,14 +119,17 @@
     /*设置定位方式为绝对定位，且设置较高的z-index值，使其浮于其他元素上方*/
     position: absolute; z-index: 200;
     /*设置元素大小*/
-    width: 100%; height: 4rem; -webkit-box-sizing: border-box; -moz-box-sizing: border-box; box-sizing: border-box;
+    width: 100%; height: 3.6rem; padding:0.2rem 0 0 0;
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
     /*使元素居中显示*/
     bottom: 0;
     /*设置背景、阴影等外观样式*/
-    background: #FFF; box-shadow: 0 0.1rem 0.15rem rgba(0,0,0,.2);
+    background: #f0f0f0; box-shadow: 0 0.1rem 0.15rem rgba(0,0,0,.2);
   }
-  .popup-content button{position: absolute; bottom: 0; color: #FFF; background: #E74C3C;
-    width: 100%; border: 0; font-size: 0.4rem; padding: 0.1rem 0.5rem;}
+  .close {position: absolute; bottom: 0; width: 100%; height: 0.7rem; border: 0; font-size: 0.3rem;
+    padding: 0 0.5rem; color: #000000; background-color: #ffffff;}
   .popup-bg{ position: relative; z-index: 100; width: 100%; height: 100%; background: rgba(0,0,0,.3); }
 </style>
 
