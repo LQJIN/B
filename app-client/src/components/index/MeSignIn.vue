b3022e77bc54dc9a68138f447c3b581560407ddd
LQJIN <isjinliqiang@foxmail.com>
2018-05-13 21:09:30 +0800

    change icon & cahnge some function

--- app-client/src/components/index/MeSignIn.vue
+++ app-client/src/components/index/MeSignIn.vue
@@ -1,29 +1,18 @@
 <template>
   <div class="me-sign-in">
     <me-head></me-head>
-      <div class="sign-in-out">
-        <h4>SIGN IN</h4><button>SIGN UP</button>
-      </div>
     <div class="sign-content" id="sign-content">
       <div class="input-button">
-
-        <div class="name"><span class="icon-tablet"></span>
+        <div class="name"><span class="fa fa-user-circle-o"></span>
           <input type="text" name="name" placeholder="手机号" v-model="tel" @blur="check_tel"/>
         </div>
-        <div class="pass"><span class="icon-lock"></span>
+        <div class="pass"><span class="fa fa-key"></span>
           <input type="password" name="pass" placeholder="密码" v-model="password"/>
-        </div>
-        <!--<div class="pass">
-          <input type="password" style="height: 1rem;margin-top: 0.25rem;width: 100%;" placeholder="请确认密码" v-model="repass">
-        </div>-->
-        <div class="keep-sign">
-          <span v-on:click="toggle()" v-bind:class="{'icon-check':isCheck,'icon-check-empty':!isCheck}"></span>
-          <p>Keep me signed in</p>
         </div>
 
         <!--<input class="button" type="submit" value="Continue"/>-->
         <!--<router-link to="/memainpage"><button class="button" style="line-height: 0.6rem; border: none; background: #89a1d1; color: #ffffff;  width: 5.8rem; font-size: 0.25rem;" v-on:click="check">continue</button></router-link>-->
-        <router-link to="/memainpage"><button class="button" >continue</button></router-link>
+        <router-link to="/memainpage"><button class="button" >Sign in</button></router-link>
 
       </div>
       <div class="forget-password">
@@ -39,32 +28,32 @@
           <div class="clear-both"></div>
       </div>-->
     </div>
-    <foot v-bind:class="{'isMe':isNowPage}"></foot>
+    <!--<foot v-bind:class="{'isMe':isNowPage}"></foot>-->
   </div>
 </template>
 
 <style scoped>
   /*input背景提示文字颜色*/
-  ::-webkit-input-placeholder { color:#ffffff; font-size: 0.25rem; }
-  ::-moz-placeholder { color:#ffffff; font-size: 0.25rem;} /* firefox 19+ */
-  :-ms-input-placeholder { color:#ffffff; font-size: 0.25rem;} /* ie */
-  input:-moz-placeholder { color:#ffffff; font-size: 0.25rem;}
+  ::-webkit-input-placeholder { color:#c9c9c9; font-size: 0.25rem; }
+  ::-moz-placeholder { color:#c9c9c9; font-size: 0.25rem;} /* firefox 19+ */
+  :-ms-input-placeholder { color:#c9c9c9; font-size: 0.25rem;} /* ie */
+  input:-moz-placeholder { color:#c9c9c9; font-size: 0.25rem;}
 
   .me-sign-in{ margin-top: 1rem;}
-  .sign-in-out{ height: 1rem; text-align: center; display: flex; background: #002a76; color: #ffffff;}
-  .sign-in-out h4{  flex: 1;  text-indent: 1.6rem; line-height: 1rem; font-size: 0.35rem;}
-  .sign-in-out button{ width: 1.3rem; height: 0.6rem; line-height: 0.6rem; margin: 0.2rem 0.1rem 0.2rem 0;
-    background: #2ac395; font-size: 0.25rem; color: #ffffff; border: none;}
 
-  .sign-content{padding: 0 0.3rem; width:5.8rem; position: absolute; top:2rem; bottom: 0; text-align: center; background:
-    -webkit-linear-gradient(#1649a9, #dadada); margin: 0 0 0.7rem 0;}
-  .sign-content span{ width: 0.6rem; height:0.6rem; line-height: 0.6rem; display: block; float: left; color: #ffffff; font-size: 0.4rem;}
-  .sign-content input{ width: 5rem;height: 0.6rem; font-size: 0.25rem; color: #ffffff;
+  .sign-content{padding: 0 0.3rem; width:5.8rem; position: absolute; top:0.6rem; bottom: 0; text-align: center;
+    /*background: -webkit-linear-gradient(#1649a9, #dadada);渐变背景色*/ background: #ffffff; }
+
+  .sign-content span{ width: 0.6rem; height:0.6rem; line-height: 0.6rem; display: block; float: left;
+    color: #c9c9c9; font-size: 0.3rem;}
+
+  .sign-content input{ width: 5rem;height: 0.6rem; font-size: 0.25rem; color: #c9c9c9;
     line-height: 0.6rem; border: none; background: transparent; outline: none; }
   .input-button{text-align: left;}
-  .input-button div{ border-bottom: 0.01rem solid #ffffff; padding: 0.2rem 0 0.1rem 0;}
+  .input-button div{ border-bottom: 0.01rem solid #e7e7e7; padding: 0.2rem 0 0.1rem 0;}
 
-  .sign-content .button{line-height: 0.6rem; border: none; background: #89a1d1; color: #ffffff;  width: 5.8rem; font-size: 0.25rem;}
+  .button{line-height: 0.6rem; border: none; background: #89a1d1; color: #ffffff;
+    width: 5.8rem; font-size: 0.25rem;}
   .sign-content .keep-sign{border: none; color:#ffffff; font-size: 0.25rem;}
   .sign-content .keep-sign p{display: block; width: 5rem; float: right;}
   .forget-password{text-align: center; border: none; height: 1rem; line-height: 1rem; font-size: 0.25rem;}
@@ -100,9 +89,6 @@
     },
 
     methods:{
-      toggle: function () {
-        this.isCheck = !this.isCheck;
-      },
       /*check:function () {
         Axios.get('http://localhost:3000/').then((response) => {
           console.log(response.data);
