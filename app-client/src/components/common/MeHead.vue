a590d734117f40a20c67a9539b5f306d51d71e3d
LQJIN <isjinliqiang@foxmail.com>
2018-05-14 10:42:09 +0800

    change style

--- app-client/src/components/common/MeHead.vue
+++ app-client/src/components/common/MeHead.vue
@@ -19,9 +19,9 @@
   }
 </script>
 <style scoped>
-  .me-head { display: flex; height: 0.6rem; color: #fff; position: fixed; top: 0; width: 100%;
+  .me-head { display: flex; height: 0.8rem; color: #fff; position: fixed; top: 0; width: 100%;
               background: #ffffff; border-bottom: 0.01rem solid #e7e7e7; z-index: 1;}
-  .me-head span{ width: 0.8rem; height: 0.6rem; line-height: 0.6rem; text-align:center; font-size: 0.3rem; color: #c9c9c9; }
-  .me-head h2 { flex: 1; text-align: center; text-indent: 0rem; line-height: 0.6rem; font-size: 0.3rem; color: #000000;}
+  .me-head span{ width: 0.8rem; height: 0.8rem; line-height: 0.8rem; text-align:center; font-size: 0.3rem; color: #c9c9c9; }
+  .me-head h2 { flex: 1; text-align: center; text-indent: 0rem; line-height: 0.8rem; font-size: 0.3rem; color: #000000;}
 </style>
 
