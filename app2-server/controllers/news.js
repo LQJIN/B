abbd8de1b0976a1d661c7a3ab14919e936d7e209
LQJIN <isjinliqiang@foxmail.com>
2018-05-17 10:11:14 +0800

    change remark

--- app2-server/controllers/news.js
+++ app2-server/controllers/news.js
@@ -4,7 +4,8 @@ var news_model = require('../models/news_model.js');
 exports.index = function (req,res,next) {
     /*res.render('index',{title:'Express'});*/
     //res.send('跨域请求，请求所有的文章数据。');
-    news_model.getAllNews (function (results) {
+    //var length = req.query.length;//获取请求length条数据的长度
+    news_model.getAllNews ( function (results) {
         return res.json(results);
     });
 };
