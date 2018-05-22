9fc56d3cd4fbeb9258bb22a341292fca13fb30a3
LQJIN <isjinliqiang@foxmail.com>
2018-05-22 10:00:42 +0800

    add check_account methods

--- app2-server/controllers/user.js
+++ app2-server/controllers/user.js
@@ -1,12 +1,11 @@
 var user_model = require('../models/user_model');
 
-exports.index = function (req,res,next) {
-/*res.render('index',{title:'Express'});*/
-    res.send('跨域请求，请求所有的文章数据。');
-};
-
-exports.reg = function (req,res,next) {
-    user_model.insertUser(function (result) {
-        console.log(result);
+/*检查用户名是否存在*/
+exports.check_account = function (req,res,next) {
+    /*res.render('index',{title:'Express'});*/
+    var Account = req.query.Account;//获取要查询的账户名
+    user_model.checkAccount (Account, function (result) {
+        //return res.send(result);
+        return res.json(result);
     });
 };
