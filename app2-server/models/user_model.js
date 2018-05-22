c94fb9907f47f71740aacdf530b0b377595dd565
LQJIN <isjinliqiang@foxmail.com>
2018-05-22 21:21:20 +0800

    add checkAccount methods

--- app2-server/models/user_model.js
+++ app2-server/models/user_model.js
@@ -4,3 +4,8 @@ exports.checkAccount = function (Account, callback) {
     var sql = "select count(user_id) from users where account = ? ";
     db.query(sql,[Account],callback);
 };
+
+exports.insertAccount = function (account, password, callback) {
+    var sql = "insert into users(account,password) values (?,?)";
+    db.query(sql, [account,password], callback);
+};
