4fc2da1c7c7d3a8ceab726eb40b605a2fa65bde8
LQJIN <isjinliqiang@foxmail.com>
2018-05-22 21:20:23 +0800

    add post insert account methods

--- app2-server/routes/users.js
+++ app2-server/routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 var user = require('../controllers/user.js');
 
-/* Check the account exists or not . */
+/* check the account exists or not . */
 router.get('/check_account', user.check_account);
+
+/* insert account . */
+router.post('/insert_account', user.insert_account);
 
 module.exports = router;
