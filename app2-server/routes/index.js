53f239628bec6fcb99975e26f7149936af307d68
LQJIN <isjinliqiang@foxmail.com>
2018-05-22 10:02:31 +0800

    change router for the only /index/..

--- app2-server/routes/index.js
+++ app2-server/routes/index.js
@@ -1,18 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-var user = require('../controllers/user.js');
 var news = require('../controllers/news.js');
 
 
-/* GET home page. */
+/* GET home page */
 router.get('/index', news.index);
 
 /* GET news detail page */
 router.get('/newsDetail', news.detail);
 
-
-/* GET reg page. */
-router.get('/reg',user.reg);
 
 module.exports = router;
