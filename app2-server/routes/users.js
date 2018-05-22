660f11a055e86efdcf90b65ec6c33910817eb560
LQJIN <isjinliqiang@foxmail.com>
2018-05-22 10:05:48 +0800

    add router /check_account check account exist or not

--- app2-server/routes/users.js
+++ app2-server/routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var user = require('../controllers/user.js');
+
+/* Check the account exists or not . */
+router.get('/check_account', user.check_account);
 
 module.exports = router;
