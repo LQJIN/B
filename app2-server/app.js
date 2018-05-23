7c080d2098135004a5648ac18e984049ea5aaaba
LQJIN <isjinliqiang@foxmail.com>
2018-05-23 15:29:58 +0800

    add express-session,use cookieParser&express-session

--- app2-server/app.js
+++ app2-server/app.js
@@ -5,11 +5,26 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');/*引入cors包解决跨域*/
-
+var session = require('express-session');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+/*配置cors*/
+app.use(cors({
+    origin:'http://localhost:8088',//允许域访问
+    methods:['GET','POST'],//只允许get和post请求
+    credentials:true,//是否带cookie
+    /*header:'Content-Type, Content-Length, Authorization, Accept, X-Requested-With'*/
+}));
+
+app.use(cookieParser('sessiontest'));
+app.use(session({
+    secret: 'sessiontest',//与cookieParser中的一致
+    resave: true,
+    saveUninitialized:true
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,35 +38,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-/*配置cors*/
-app.use(cors({
-    origin:'http://localhost:8088',//允许域访问
-    methods:['GET','POST'],//只允许get和post请求
-    credentials:true,//是否带cookie
-    /*header:'Content-Type, Content-Length, Authorization, Accept, X-Requested-With'*/
-}));
-
-
-/*app.use('*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8088');
-    res.header('Access-Control-Allow-Headers', 'Content-Type,Content-Length, Authorization,\'Origin\',Accept,X-Requested-With');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('X-Powered-By', ' 3.2.1');
-    res.header('Content-Type', 'application/json;charset=utf-8');
-    /!*if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }*!/
-    next();
-});*/
-
-
 app.use('/', index);
 app.use('/users', users);
-
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
