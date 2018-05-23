68e82473fa70af17d19ada3d0e4b4aecd18f2cf5
LQJIN <isjinliqiang@foxmail.com>
2018-05-23 17:31:04 +0800

    add post /sign_in

--- app2-server/routes/users.js
+++ app2-server/routes/users.js
@@ -6,7 +6,12 @@ var user = require('../controllers/user.js');
 /* check the account exists or not . */
 router.get('/check_account', user.check_account);
 
-/* insert account . */
+/* insert account . 只能够用post方法*/
 router.post('/insert_account', user.insert_account);
+/*router.get('/insert_account', user.insert_account);*/
+
+/* sign in . 只能够用post方法*/
+router.post('/sign_in', user.sign_in);
+/*router.get('/sign_in', user.sign_in);*/
 
 module.exports = router;
