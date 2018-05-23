052cafd99a0847b8f4da8402ba8d504ad38323ae
LQJIN <isjinliqiang@foxmail.com>
2018-05-23 17:32:24 +0800

    add signIn function

--- app2-server/models/user_model.js
+++ app2-server/models/user_model.js
@@ -9,3 +9,8 @@ exports.insertAccount = function (account, password, callback) {
     var sql = "insert into users(account,password) values (?,?)";
     db.query(sql, [account,password], callback);
 };
+
+exports.signIn = function (account, password, callback) {
+    var sql = "select * from users where account = ? and password = ?";
+    db.query(sql, [account,password], callback);
+};
