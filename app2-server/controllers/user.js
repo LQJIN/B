5457ee7336f04e6812e04415cd94d6c395ac23ba
LQJIN <isjinliqiang@foxmail.com>
2018-05-28 20:34:33 +0800

    add&use account_by_id methods add session in signin methods

--- app2-server/controllers/user.js
+++ app2-server/controllers/user.js
@@ -16,7 +16,6 @@ exports.insert_account = function (req, res, next) {
     var account = req.body.account;//获取账户名
     var password = req.body.password;//获取密码
     user_model.insertAccount (account, password, function (result) {
-        req.session.id = result[0].userid;
         return res.json(result);
     });
 };
@@ -25,6 +24,17 @@ exports.sign_in = function (req, res, next) {
     var account = req.body.account;//获取账户名
     var password = req.body.password;//获取密码
     user_model.signIn (account, password, function (result) {
+        //res.cookie("account", {account: result}, {maxAge: 60000});
+        req.session.id = result[0].userid;
+        //console.log(result);
+        return res.json(result);
+    });
+};
+
+exports.account_by_id = function (req,res,next) {
+    var accountId = req.body.id;//获取要查询的id
+    user_model.accountById (accountId, function (result) {
+        //return res.send(result);
         return res.json(result);
     });
 };
