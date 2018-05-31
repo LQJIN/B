d44e14667bc676d89a66aea9db8bed8ef242470a
LQJIN <isjinliqiang@foxmail.com>
2018-05-31 10:32:01 +0800

    add remarks & server methods

--- app2-server/models/user_model.js
+++ app2-server/models/user_model.js
@@ -1,21 +1,24 @@
 var db = require('./db.js');
-
+/*client*/
 exports.checkAccount = function (Account, callback) {
     var sql = "select count(user_id) from user where account = ? ";
     db.query(sql,[Account],callback);
 };
-
 exports.insertAccount = function (account, password, callback) {
     var sql = "insert into user(account,password) values (?,?)";
     db.query(sql, [account,password], callback);
 };
-
 exports.signIn = function (account, password, callback) {
     var sql = "select userid from user where account = ? and password = ?";
     db.query(sql, [account,password], callback);
 };
-
 exports.accountById = function (accountId, callback) {
     var sql = "select * from user where userid = ? ";
     db.query(sql, [accountId], callback);
+};
+
+/*server*/
+exports.getNameByPassword = function (servername,password,callback) {
+    var sql = "select * from serveruser where servername = ? and password = ? ";
+    db.query(sql,[servername,password],callback);
 };
