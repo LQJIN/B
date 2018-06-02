61d05e50bfeccd0ebf2af060904ad63c6abb35f3
LQJIN <isjinliqiang@foxmail.com>
2018-06-02 19:30:12 +0800

    change head class name

--- app-client/src/components/common/Head.vue
+++ app-client/src/components/common/Head.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="head">
-    <ul>
+    <ul class="head-ul">
       <li><p class="fa">NewsDaily</p></li>
       <li><span class="fa fa-search"></span></li>
       <li><router-link to="/personal"><span class="fa fa-user"></span></router-link></li>
      <!-- <li><router-link to="/signin" ><span class="fa fa-user"></span></router-link></li>-->
     </ul>
+
   </div>
 </template>
 
@@ -14,15 +15,15 @@
   .head { display: flex; height: 0.6rem; line-height: 0.6rem;
     position: fixed; top: 0; width: 100%; background-color: #ffffff;}
 
-  .head ul {display: flex; width: 100%;}
-  .head ul li {text-align: center;}
-  .head ul li:nth-child(1){flex: 8; float: left; text-align: left;
+  .head-ul {display: flex; width: 100%;}
+  .head-ul li {text-align: center;}
+  .head-ul li:nth-child(1){flex: 8; float: left; text-align: left;
     font-size: 0.25rem; padding-left: 0.3rem; }
-  .head ul li:nth-child(2){flex: 1; float: right; }
-  .head ul li:nth-child(3){flex: 1; float: right; }
+  .head-ul li:nth-child(2){flex: 1; float: right; }
+  .head-ul li:nth-child(3){flex: 1; float: right; }
 
-  .head ul li .fa { color: #c9c9c9;}
+  .head-ul li .fa { color: #c9c9c9;}
 
-  .head span{ width: 0.6rem; height: 0.6rem; line-height: 0.6rem;
+  .head-ul span{ width: 0.6rem; height: 0.6rem; line-height: 0.6rem;
     text-align:center; float: right; font-size: 0.25rem; color: #ffffff; }
 </style>
