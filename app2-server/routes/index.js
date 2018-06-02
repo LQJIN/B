9e8f7018b6961e2b48cdc1173d0b76c1c3bfa6ba
LQJIN <isjinliqiang@foxmail.com>
2018-06-02 19:26:25 +0800

    add news tag methods

--- app2-server/routes/index.js
+++ app2-server/routes/index.js
@@ -6,9 +6,20 @@ var news = require('../controllers/news.js');
 
 /* GET all news page */
 router.get('/index', news.index);
-
+/* GET tag */
+router.get('/tag', news.tag);
+/* GET getNewsByTagId
+router.get('/getNewsByTagId', news.getNewsByTagId);*/
 /* GET news detail page */
 router.get('/newsDetail', news.detail);
+
+/* GET news newsChina page */
+router.get('/newsChina', news.newsChina);
+router.get('/newsWorld', news.newsWorld);
+router.get('/newsBusiness', news.newsBusiness);
+router.get('/newsSports', news.newsSports);
+router.get('/newsOpinion', news.newsOpinion);
+router.get('/newsLife', news.newsLife);
 
 
 module.exports = router;
