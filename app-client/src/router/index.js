a2a057b312c47d6357961304b3b5da0befb4a140
LQJIN <isjinliqiang@foxmail.com>
2018-06-03 11:22:13 +0800

    add redirect router

--- app-client/src/router/index.js
+++ app-client/src/router/index.js
@@ -22,6 +22,10 @@ export default new Router({
   routes: [
     {
       path: '/',
+      redirect:'/Home'
+    },
+    {
+      path: '/',
       name: 'Index',
       component: Index,
       children: [
