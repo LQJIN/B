9f9bf735972f882e0bd4bc1e6301aa2f2b8dc1ac
LQJIN <isjinliqiang@foxmail.com>
2018-06-03 11:39:07 +0800

    router check before new page

--- app-client/src/main.js
+++ app-client/src/main.js
@@ -22,7 +22,19 @@ Vue.config.productionTip = false;
 
 Vue.use(MintUI);
 
-
+router.beforeEach((to,from,next) => {//路由校验
+  if(to.matched.some( m => m.meta.auth)){
+  // 对路由进行验证
+    if(store.state.isLogin === '100') { // 已经登陆
+      next();  // 正常跳转到你设置好的页面
+    }else{
+    // 未登录则跳转到登陆界面，query:{ Rurl: to.fullPath}表示把当前路由信息传递过去方便登录后跳转回来；
+      next({path:'/signin',query:{ Rurl: to.fullPath} });
+    }
+  }else{
+      next();
+  }
+});
 
 /* eslint-disable no-new */
 new Vue({
